1206ee10a4e615fa3b0d23d214554ed6dca515e1
kotori-web-solutions <info@kotori.de>
2014-02-18 02:37:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+New year's greeting card 2014 with WebGL/Three.js
+==============
+
+Just a little finger exercise fitting the occasion then :) Enjoy and build on it.
+
+Notes:
+
+- TV Tower model and texture by [Thomas Laubner](http://sketchup.google.com/3dwarehouse/details?mid=c6adcf91ad165f9c19c095d6e94545e)
+- Building and floor textures from [freetextures.3dtotal.com](http://freetextures.3dtotal.com/)
+- Background skybox graphics done with [Spacescape app by Alex Peterson](http://sourceforge.net/projects/spacescape/)
---
+++ sorry.php
@@ -0,0 +1,53 @@
+
+
+<html>
+<head>
+	
+	<title>Happy New Year 2014! - kotori web solutions Maren Arnhold</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=0;" />
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+	<script src="./jquery.js"></script>
+	<script src="./jquery.percentageloader-0.1.min.js"></script>
+	<script src="./three.min.js"></script>
+	<link rel="stylesheet" href="jquery.percentageloader-0.1.css" type="text/css" />
+	<style>
+	
+	body {
+	  margin: 0px;
+	  overflow: hidden;
+	  font-size:16px;z-index:100;position:absolute;top:20px;padding:1em;font-family:Ubuntu,Arial,sans-serif;text-align:center;
+	  
+	}
+	
+	
+	a { color:#444; text-decoration: none; font-weight: bold; }
+	
+	</style>	
+</head>
+<body>
+
+Diese Grußkarte benötigt WebGL, eine fortschrittliche Webtechnologie für 3D-Darstellungen. Leider unterstützt Ihr Browser diese Technologie noch nicht. Eine Übersicht, welche Plattformen WebGL bereits implementiert haben, finden Sie unter:<br /><br />
+
+<a href="http://caniuse.com/webgl">http://caniuse.com/webgl</a><br /><br />
+
+Alternativ können Sie sich diese Grußkarte auch bei YouTube ansehen:<br /><br />
+
+<a href="http://www.youtube.com/watch?v=1q4r5yKe488">http://www.youtube.com/watch?v=1q4r5yKe488</a>
+<br /><br /><br /><br />
+
+This greeting card requires WebGL, an advanced web technology for 3D display. Unfortunately, your browser does not support this technology yet. Please consult the following website for more information about suitable platforms:<br /><br />
+
+<a href="http://caniuse.com/webgl">http://caniuse.com/webgl</a><br /><br />
+
+Alternatively, you can watch this greeting card as a video on YouTube:<br /><br />
+
+<a href="http://www.youtube.com/watch?v=1q4r5yKe488">http://www.youtube.com/watch?v=1q4r5yKe488</a>
+<br /><br />
+		
+</body>
+
+</html>
+
+
+
